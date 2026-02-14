f5236a41adf518de799fee946175a24d4ffb6811
telusnba <46972682+telusnba@users.noreply.github.com>
2026-02-14 13:11:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_KEY = os.getenv("API_KEY")
-API_USER = os.getenv("API_USER")
-BASE_URL = f"https://api12.scamalytics.com/v3/{API_USER}/"
+
+API_KEY1 = os.getenv("API_KEY1")
+API_USER1 = os.getenv("API_USER1")
+
+API_KEY2 = os.getenv("API_KEY2")
+API_USER2 = os.getenv("API_USER2")
+
+API_KEY3 = os.getenv("API_KEY3")
+API_USER3 = os.getenv("API_USER3")
+
+API_KEY4 = os.getenv("API_KEY4")
+API_USER4 = os.getenv("API_USER4")
