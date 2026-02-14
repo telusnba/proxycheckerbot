d561dcd3cf9f1bdb69d3faf6d4348a7127cef2ea
telusnba <46972682+telusnba@users.noreply.github.com>
2026-02-14 13:21:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ API_USER1 = os.getenv("API_USER1")
 
 API_KEY2 = os.getenv("API_KEY2")
 API_USER2 = os.getenv("API_USER2")
-
-API_KEY3 = os.getenv("API_KEY3")
-API_USER3 = os.getenv("API_USER3")
-
-API_KEY4 = os.getenv("API_KEY4")
-API_USER4 = os.getenv("API_USER4")
+# 
+# API_KEY3 = os.getenv("API_KEY3")
+# API_USER3 = os.getenv("API_USER3")
+# 
+# API_KEY4 = os.getenv("API_KEY4")
+# API_USER4 = os.getenv("API_USER4")
